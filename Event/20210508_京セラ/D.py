8eb45be4aa3948b0aeaa46f6324d9c8807155582
goutarouh <gouta.gorogoro@gmail.com>
2021-05-10 22:55:04 +0900

    bit全探索ok

--- Event/20210508_京セラ/D.py
+++ Event/20210508_京セラ/D.py
@@ -1,15 +1,28 @@
 from sys import stdin
 
-
-def div2(n):
-    return n % 200
-
-
 N = int(stdin.readline().rstrip())
 
-A = list(map(div2, map(int, stdin.readline().rstrip().split())))
-
-A = sorted(set(A), key=A.index)
+A = list(map(lambda x: x % 200, map(int, stdin.readline().rstrip().split())))
 
 
-print(A)
+def intToS(bit):
+    S = []
+    for i in range(N):
+        if bit & 1 << i:
+            S.append(i)
+    return S
+
+
+seen = [[] for _ in range(200)]
+for bit in range(1 << N):
+    S = intToS(bit)
+    ans = sum([A[i] for i in S]) % 200
+
+    if seen[ans]:
+        print("Yes")
+        print(len(seen[ans]), *[x + 1 for x in seen[ans]], sep=" ")
+        print(len(S), *[x + 1 for x in S], sep=" ")
+        exit()
+
+    seen[ans] = S
+print("No")
